5a5c5e9db0e93790037d326083ea51c43e649e0d
tanvir <tanvirtmm@gmail.com>
2025-07-17 10:57:21 +0600

    part 2 done

--- helpers/environment.js
+++ helpers/environment.js
@@ -1,34 +1,55 @@
-/*
- * Title: Environments
- * Description: Handle All Environment related things
- * Author: Md. Tanvir Arif Siddiqui
- * Date: 7/1/2025
- * Time: 1:02 PM
- */
+//Module Scaffolding
 
-// dependencies
+const environments = {};
 
-// module scaffolding
-const environment = {};
-
-environment.staging = {
+environments.staging = {
     port: 3000,
-    envName: 'staging',
-};
+    envName: 'staging'
+}
 
-environment.production = {
+environments.production = {
     port: 5000,
-    envName: 'production',
-};
+    envName: 'production'
+}
 
-// determine which environment was passed
-const currentEnvironment =
-    typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+//determine which environment was passed
+const currentEnvironment = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : 'staging';
 
-// export corresponding object
-const environmentToExport =
-    typeof environment[currentEnvironment] === 'object'
-        ? environment[currentEnvironment]
-        : environment.staging;
-
+//export corresponding environment object
+const environmentToExport = typeof (environments[currentEnvironment]) === 'object' ? environments[currentEnvironment] : environments.staging;
 module.exports = environmentToExport;
+
+// /*
+//  * Title: Environments
+//  * Description: Handle All Environment related things
+//  * Author: Md. Tanvir Arif Siddiqui
+//  * Date: 7/1/2025
+//  * Time: 1:02 PM
+//  */
+//
+// // dependencies
+//
+// // module scaffolding
+// const environment = {};
+//
+// environment.staging = {
+//     port: 3000,
+//     envName: 'staging',
+// };
+//
+// environment.production = {
+//     port: 5000,
+//     envName: 'production',
+// };
+//
+// // determine which environment was passed
+// const currentEnvironment =
+//     typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+//
+// // export corresponding object
+// const environmentToExport =
+//     typeof environment[currentEnvironment] === 'object'
+//         ? environment[currentEnvironment]
+//         : environment.staging;
+//
+// module.exports = environmentToExport;
--- index.js
+++ index.js
@@ -9,22 +9,41 @@
 // dependencies
 const http = require('http');
 const {handleRequestResponse} = require('./helpers/handleRequestResponse');
+const environment = require('./helpers/environment');
+const data = require('./lib/data');
+
 
 // app object module Scaffolding
 
 const app = {};
 
-//configuration
+//testing file system
+// data.create("test", "newFile", {"country": "Bangladesh", "language": "Bengali"}, (err) => {
+//     if (err) {
+//         console.log(`Error is : ${err}`);
+//     }
+// });
 
-app.config = {
-    port: 3000
-};
+// data.read("test", "newFile", (err, data) => {
+//     if (!err) {
+//         console.log(data);
+//     } else console.log(err);
+// })
+
+// data.update("test", "newFile", {"country": "America", "language": "English"}, (err) => {
+//     console.log(`Error is : ${err}`);
+// });
+
+data.delete("test", "newFile", (err) => {
+    console.log(err);
+})
 
 
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`Server listening on port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`The Environment Variable is ${process.env.NODE_ENV}`);
+        console.log(`Server listening on port ${environment.port}`);
     });
 };
 
